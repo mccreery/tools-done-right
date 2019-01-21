91647348093bc382d1f087b4ce9fec9f08ac9db1
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2019-01-21 23:41:40 +0000

    Add basic mcmod.info and base class

---
+++ src/main/java/jobicade/toolsdoneright/ToolsDoneRight.java
@@ -0,0 +1,27 @@
+package jobicade.toolsdoneright;
+
+import org.apache.logging.log4j.Logger;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = ToolsDoneRight.MODID, name = "Tools Done Right", version = "2.0-alpha")
+public class ToolsDoneRight {
+    public static final String MODID = "toolsdoneright";
+
+    private static Logger logger;
+    public static Logger getLogger() { return logger; }
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent e) {
+        logger = e.getModLog();
+        logger.info("PreInit complete.");
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent e) {
+        logger.info("Init complete.");
+    }
+}

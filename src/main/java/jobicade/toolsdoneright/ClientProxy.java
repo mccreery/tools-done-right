2b4a9b69fc0c1fca91557b98f88ceb5b28168b2b
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2019-01-22 17:18:47 +0000

    Remove unncessary proxies

--- src/main/java/jobicade/toolsdoneright/ClientProxy.java
+++
@@ -1,8 +0,0 @@
-package jobicade.toolsdoneright;
-
-public class ClientProxy extends CommonProxy {
-    @Override
-    public void onClient(Runnable runnable) {
-        runnable.run();
-    }
-}
--- src/main/java/jobicade/toolsdoneright/ToolsDoneRight.java
+++ src/main/java/jobicade/toolsdoneright/ToolsDoneRight.java
@@ -3,16 +3,12 @@ package jobicade.toolsdoneright;
 import org.apache.logging.log4j.Logger;
 
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = ToolsDoneRight.MODID, name = "Tools Done Right", version = "2.0-alpha")
 public class ToolsDoneRight {
     public static final String MODID = "toolsdoneright";
-
-    @SidedProxy(clientSide = "jobicade.toolsdoneright.ClientProxy", serverSide = "jobicade.toolsdoneright.CommonProxy")
-    public static CommonProxy proxy;
 
     private static Logger logger;
     public static Logger getLogger() { return logger; }
